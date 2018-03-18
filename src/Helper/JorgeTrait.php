66ddedcf924e430199d2d9edd4d111f0e5606915
Jason Proctor <jproctor@mtholyoke.edu>
2018-03-18 11:18:47 -0400

    Create JorgeTrait for common functions

---
+++ src/Helper/JorgeTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MountHolyoke\Jorge\Helper;
+
+use Psr\Log\LogLevel;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Parent class for Jorge commands to collect some common functionality.
+ */
+trait JorgeTrait{
+  protected $jorge = NULL;
+  protected $verbosity = OutputInterface::VERBOSITY_NORMAL;
+
+  /**
+   * Establish some properties that are common to all Jorge commands and tools.
+   *
+   * This should be called from the initialize() method in each. Note that for
+   * tools it’s called during setup phase, but commands don’t call it until run().
+   *
+   * @param InputInterface
+   * @param OutputInterface
+   */
+  protected function initializeJorge(InputInterface $input, OutputInterface $output) {
+    $this->jorge = $this->getApplication();
+    $this->verbosity = $output->getVerbosity();
+  }
+
+  /**
+   * Sends a message prefixed with command name to the application’s logger.
+   *
+   * @param string|NULL what log level to use, or NULL to ignore.
+   * @param string the message
+   * @param array variable substitutions for the message
+   */
+   protected function log($level, $message, array $context = []) {
+     if ($level !== NULL) {
+       $message = '{' . $this->getName() . '} ' . $message;
+       $this->jorge->log($level, $message, $context);
+     }
+   }
+
+
+}
