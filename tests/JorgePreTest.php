63c1d65461e92d8b24d4b0d35f2957b058eca89c
Jason Proctor <jproctor@mtholyoke.edu>
2018-04-01 10:03:52 -0400

    Add JorgeTests namespace

--- tests/JorgePreTest.php
+++ tests/JorgePreTest.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types = 1);
 
+namespace MountHolyoke\JorgeTests;
+
 use MountHolyoke\Jorge\Jorge;
 use PHPUnit\Framework\TestCase;
 use Spatie\TemporaryDirectory\TemporaryDirectory;
