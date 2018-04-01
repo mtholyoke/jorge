af7d695ada46b9efd01a78b2cfc876a20bc8921d
Jason Proctor <jproctor@mtholyoke.edu>
2018-03-31 21:52:59 -0400

    Refactor config dir and chdir to setUp(), add more tests

--- tests/JorgePreTest.php
+++ tests/JorgePreTest.php
@@ -15,6 +15,8 @@ final class JorgePreTest extends TestCase {
     $output = $this->jorge->getOutput();
     $output->setVerbosity(OutputInterface::VERBOSITY_QUIET);
     $this->tempDir = (new TemporaryDirectory())->create();
+    mkdir($this->tempDir->path() . DIRECTORY_SEPARATOR . '.jorge');
+    chdir($this->tempDir->path());
   }
 
   protected function tearDown() {
@@ -26,9 +28,25 @@ final class JorgePreTest extends TestCase {
    */
   public function testNoDotJorge(): void {
     $root = realpath($this->tempDir->path());
-    chdir($root);
+    rmdir($root . DIRECTORY_SEPARATOR . '.jorge');
     $this->jorge->configure();
     $this->assertNull($this->jorge->getPath());
+  }
+
+  /**
+   * Finds the project root from a random subdirectory.
+   */
+  public function testFindRootPath(): void {
+    $root = realpath($this->tempDir->path());
+    $depth = 2 + random_int(0, 4);
+    $path = $root;
+    for ($i = 0; $i < $depth; $i++) {
+      $path .= DIRECTORY_SEPARATOR . bin2hex(random_bytes(4));
+      mkdir($path);
+    }
+    chdir($path);
+    $this->jorge->configure();
+    $this->assertSame($root, $this->jorge->getPath());
   }
 
   /**
@@ -36,19 +54,43 @@ final class JorgePreTest extends TestCase {
    */
   public function testEmptyDotJorge(): void {
     $root = realpath($this->tempDir->path());
-    mkdir($root . DIRECTORY_SEPARATOR . '.jorge');
-    chdir($root);
     $this->jorge->configure();
     $this->assertSame($root, $this->jorge->getPath());
     $this->assertSame([], $this->jorge->getConfig());
   }
 
+  /**
+   * Generate random subdirectory.
+   */
+  public function testGetPath(): void {
+    $root = realpath($this->tempDir->path());
+    $randir = bin2hex(random_bytes(4));
+    $subdir = $root . DIRECTORY_SEPARATOR . $randir;
+    mkdir($subdir);
+    $this->jorge->configure();
+    $this->assertSame($subdir, $this->jorge->getPath($randir));
+    $this->assertSame($root, $this->jorge->getPath($randir . 'x'));
+  }
+
+  /**
+   * Generate random config file.
+   */
+  public function testGetConfig(): void {
+    $root = realpath($this->tempDir->path());
+    $configFile = implode(DIRECTORY_SEPARATOR, [$root, '.jorge', 'config.yml']);
+    $configKey = bin2hex(random_bytes(4));
+    $configValue = bin2hex(random_bytes(4));
+    file_put_contents($configFile, "{$configKey}: {$configValue}\n");
+    $this->jorge->configure();
+    $this->assertSame($configValue, $this->jorge->getConfig($configKey));
+    $this->assertSame($configValue, $this->jorge->getConfig($configKey, 'X'));
+    $this->assertNull($this->jorge->getConfig($configKey . 'x'));
+    $this->assertSame('X', $this->jorge->getConfig($configKey . 'x', 'X'));
+  }
 
   // TODO: test pathfinding and loading config given various mocks/fixtures:
   // config['includeconfig']
-  // static findRootPath()
-  // getConfig()
-  // getPath()
+  // getPath() Exceptions
   // loadConfigFile()
   // static sanitizePath()
 
