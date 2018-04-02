2cad464d8216ac653ff56b95cb5be0c9cb44508c
Jason Proctor <jproctor@mtholyoke.edu>
2018-04-01 20:41:47 -0400

    Verify MockJorge startup and getTool()

--- tests/JorgeTest.php
+++ tests/JorgeTest.php
@@ -3,12 +3,13 @@ declare(strict_types = 1);
 
 namespace MountHolyoke\JorgeTests;
 
+use MountHolyoke\JorgeTests\MockConsoleOutput;
 use MountHolyoke\JorgeTests\MockJorge;
-use MountHolyoke\Jorge\Jorge;
+use MountHolyoke\Jorge\Tool\Tool;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LogLevel;
 use Spatie\TemporaryDirectory\TemporaryDirectory;
-use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class JorgeTest extends TestCase {
   protected $jorge;
@@ -22,22 +23,55 @@ final class JorgeTest extends TestCase {
     $root = $this->tempDir->path();
     mkdir($root . DIRECTORY_SEPARATOR . '.jorge');
     chdir($root);
+    touch('.jorge' . DIRECTORY_SEPARATOR . 'config.yml');
     $this->jorge = new MockJorge($root);
-    // $output = $this->jorge->getOutput();
-    // $output->setVerbosity(OutputInterface::VERBOSITY_QUIET);
     $this->jorge->configure();
   }
 
   protected function tearDown(): void {
+    $this->jorge = NULL;
     $this->tempDir->delete();
   }
 
   /**
-   * Verify the MockJorge is working as expected
+   * Verify the MockJorge is working as expected: that it starts the same as
+   * a typical Jorge, but with output functions replaced.
    */
   public function testMockJorge() {
-    $this->assertSame('Jorge', $this->jorge->getName());
+    # Startup messages (all verbosities):
+    $startup = [
+      [LogLevel::NOTICE, 'Project root: {%root}'],
+      [LogLevel::DEBUG,  '{composer} Executable is "{%executable}"'],
+      [LogLevel::DEBUG,  '{git} Executable is "{%executable}"'],
+      [LogLevel::NOTICE, '{git} $ {%command}'],
+      [LogLevel::DEBUG,  '{lando} Executable is "{%executable}"'],
+      ['NULL',           'Can’t read config file {%filename}'],
+    ];
+    $this->assertSame(count($startup), count($this->jorge->messages));
+    foreach ($startup as $expected) {
+      $actual = array_shift($this->jorge->messages);
+      # Paths may be different on different computers
+      array_pop($actual);
+      $this->assertSame($expected, $actual);
+    }
+    $this->assertSame(0, count($this->jorge->messages));
 
+    # From __construct():
+    $this->assertInstanceOf(MockJorge::class, $this->jorge);
+    $output = $this->jorge->getOutput();
+    $this->assertInstanceOf(MockConsoleOutput::class, $output);
+    $this->assertTrue($output->isDecorated());
+    $this->assertSame(OutputInterface::VERBOSITY_NORMAL, $output->getVerbosity());
+    $this->assertSame(0, $_ENV['SHELL_VERBOSITY']);
+
+    # From configure():
+    $this->assertSame('Jorge', $this->jorge->getName());
+    $this->assertRegExp('/\d\.\d\.\d/', $this->jorge->getVersion());
+    $this->assertSame(realpath($this->tempDir->path()), $this->jorge->getPath());
+    $this->assertSame([], $this->jorge->getConfig());
+    // $this->assertSame('jorge', $this->jorge->getConfig('appType'));
+
+    # Verify that MockJorge’s log() works as expected:
     $logLevels = [
       LogLevel::EMERGENCY,
       LogLevel::ALERT,
@@ -50,18 +84,38 @@ final class JorgeTest extends TestCase {
     ];
     foreach ($logLevels as $logLevel) {
       $logString = bin2hex(random_bytes(4));
-      $logExpect = [$logLevel, $logString, []];
+      $logExpect = [$logLevel, (string)$logString, []];
       $this->jorge->log($logLevel, $logString);
       $this->assertSame($logExpect, end($this->jorge->messages));
     }
 
+    # Verify that writeln works as expected:
     $wlnString = bin2hex(random_bytes(4));
-    $wlnExpect = ['writeln', $wlnString];
+    $wlnExpect = ['writeln', (string)$wlnString];
     $this->jorge->getOutput()->writeln($wlnString);
     $this->assertSame($wlnExpect, end($this->jorge->messages));
   }
 
+  public function testGetTool(): void {
+    # Find a name we don’t have and verify that getTool() responds correctly.
+    do {
+      $toolName = bin2hex(random_bytes(4));
+    } while (array_key_exists($toolName, $this->jorge->allTools()));
+    $this->assertNull($this->jorge->getTool($toolName));
+    $expectLog = [LogLevel::WARNING, 'Can’t get tool "{%tool}"', ['%tool' => $toolName]];
+    $this->assertSame($expectLog, end($this->jorge->messages));
+
+    # Add a tool with that name and verify that getTool() responds correctly.
+    $toolInstance = new Tool($toolName);
+    $this->jorge->addTool($toolInstance, 'echo');
+    $this->assertArrayHasKey($toolName, $this->jorge->allTools());
+    $this->assertSame($toolInstance, $this->jorge->getTool($toolName));
+
+    # This should echo the tool name without checking enablement.
+    $toolInstance->runThis($toolName);
+    $this->assertSame(['writeln', $toolName], end($this->jorge->messages));
+  }
+
   // TODO test post-config functionality
-  // getTool()
   // run()
 }
